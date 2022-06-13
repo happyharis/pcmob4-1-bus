9318ac99760c56fd5065f171a4f3f161135bb142
Haris Samingan <muhamad_haris@hotmail.com>
2022-06-13 10:55:34 +0800

    Add title

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus Arrival Time: </Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? "Loading..." : "Loaded"}
+      </Text>
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
